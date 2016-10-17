4353b0de1aa338796ee675c12379eeb18f91858e
uelio.dornelas <uelio.dornelas@lab1-dcx-28>
2016-10-17 08:55:54 -0300

    Primeiro commit

---
+++ src/Demonstracao/Teste.java
@@ -0,0 +1,15 @@
+package Demonstracao;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		int cont = 10;
+		while(cont != 0){
+			System.out.println("Aula: Eng. de software");
+			cont--;
+		}	
+
+	}
+
+}
