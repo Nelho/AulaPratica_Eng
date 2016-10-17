6957796e43c9cb8053401ca6f7406028fd8b0652
uelio.dornelas <uelio.dornelas@lab1-dcx-28>
2016-10-17 09:12:23 -0300

    Segundo Commit

--- src/Demonstracao/Teste.java
+++ src/Demonstracao/Teste.java
@@ -5,10 +5,13 @@ public class Teste {
 	public static void main(String[] args) {
 		
 		int cont = 10;
+		int inteiro = 100;
 		while(cont != 0){
 			System.out.println("Aula: Eng. de software");
+			System.out.println(inteiro);
 			cont--;
-		}	
+		}
+		
 
 	}
 
